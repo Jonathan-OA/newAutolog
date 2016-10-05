44291b1bab5f791549a96e3a54acd281fe92b3a1
Jonathan Oliveira <Jonathan Oliveira>
2016-10-04 23:36:32 -0300

    AngularGrid

--- resources/views/modulos/producao/grid.blade.php
+++ resources/views/modulos/producao/grid.blade.php
@@ -3,15 +3,27 @@
 @section('content')
     <div class="row">
         <div class="col-md-12 pad-ct">
-            <div class="panel panel-default">
+            <div class="panel panel-default" >
                 <div class="panel-heading">
                     Instalação do Sistema
                 </div>
+                <table ng-controller="ProductionCtl">
+                    <tr ng-repeat="document in documents">
+                                        {{ document.number }}
+                                    </tr>
+
+                </table>
+                
             </div>
-            {{% $documents %}}
             <button type="submit" class="btn btn-success center-block">
                         Prosseguir
                         </button>
         </div>
     </div>
 @endsection
+
+<script>
+    var ProductionCtl = function($scope){
+        $scope.documents = {{% $documents %}};
+    }
+</script>
